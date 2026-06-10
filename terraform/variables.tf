46c2380299fad4e17fc5c89e69522876200661ca
Bussy <olubusolaogoe@gmail.com>
2026-06-10 08:00:46 +0200

    feat(terraform): add variables and tfvars template

---
+++ terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "aws_region" {
+  description = "Région AWS"
+  type = string
+  default = "eu-west-3"
+}
+
+variable "project_name" {
+  description = "Préfixe utilisé pour nommer toutes les ressources"
+  type = string
+  default = "prism"
+}
+
+variable "vpc_cidr" {
+  description = "Plage d'adresses du VPC"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Plage d'adresses du sous-réseau public"
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "Type d'instance EC2"
+  type = string
+  default = "t2.micro"
+}
+
+variable "app_count" {
+  description = "Nombre de VMs applicatives"
+  type = number
+  default = 2
+}
+
+variable "ssh_public_key_path" {
+  description = "Chemin vers clé SSH publique"
+  type = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "my_ip" {
+  description = "IP publique pour l'accès SSH (format : x.x.x.x/32)"
+  type = string
+}

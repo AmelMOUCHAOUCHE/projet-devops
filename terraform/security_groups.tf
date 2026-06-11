02c80ab2e5273589979dc6d4c7109e8516a21bba
AmelMOUCHAOUCHE <amel.mouchaouche09@gmail.com>
2026-06-11 15:13:38 +0200

    feat: déploiement complet frontend Prism sur AWS

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "app" {
 }
 
 
-# Security Group - Base de données
+# Security Group - Base de donnees
 # PostgreSQL accessible uniquement depuis les VMs
 # Pas d'accès HTTP/HTTPS depuis internet
 # SSH restreint à my_ip (accès admin direct)
